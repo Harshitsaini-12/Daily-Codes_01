6653ddbc9de78211a09ce0d8689b785225915b13
Harshit <harshitsaini3582@gmail.com>
2024-06-21 14:02:13 +0530

    Time: 16 ms (96.13%), Space: 43.8 MB (82.61%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.java
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.java
@@ -1,34 +1,37 @@
 class Solution {
     public int findNumberOfLIS(int[] nums) {
         int n=nums.length;
-        int maxcount=0;
-        int maxlen=0;
-        
+
         int[]dp=new int[n+1];
         int[]count=new int[n+1];
-        
+
+        int maxCount=0;
+        int maxLen=0;
+
         for(int i=0;i<n;i++){
             dp[i]=1;
             count[i]=1;
-            
             for(int j=i-1;j>=0;j--){
                 if(nums[j]<nums[i]){
-                    if(dp[j]+1==dp[i])count[i]+=count[j];
-                    else if(dp[j]+1>dp[i]){
+                    if(1+dp[j]>dp[i]){
                         dp[i]=dp[j]+1;
                         count[i]=count[j];
+                    }else if(dp[i]==dp[j]+1){
+                        count[i]+=count[j];
                     }
                 }
             }
-            
-            if(maxlen==dp[i])maxcount+=count[i];
-            else if(maxlen<dp[i]){
-                maxlen=dp[i];
-                maxcount=count[i];
+
+            if(maxLen==dp[i])maxCount+=count[i];
+            else if(maxLen<dp[i]){
+                maxLen=dp[i];
+                maxCount=count[i];
             }
+
+
+
+
         }
-        
-         return maxcount;
-        
+        return maxCount;
     }
 }
