543b06c6219a9b51536b5ad37b2050249aea9c8a
Harshit <harshitsaini3582@gmail.com>
2025-04-07 11:07:12 +0100

    Time: 6 ms (13.7%), Space: 45.1 MB (62.83%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,17 +1,13 @@
 class Solution {
     public int minDistance_memo(String s1,String s2,int n,int m,int[][]dp){
-        if(n==0 || m==0){
-            return dp[n][m] = n != 0 ? n : m;
-        }
-        
-        if(dp[n][m]!=0){
-            return dp[n][m];
-        }
-        
+        if(n==0 || m==0)return n!=0?n:m;
+
+        if(dp[n][m]!=0)return dp[n][m];
+
         int a=minDistance_memo(s1,s2,n-1,m-1,dp);//replace
         int b=minDistance_memo(s1,s2,n-1,m,dp);//delete
         int c=minDistance_memo(s1,s2,n,m-1,dp);//insert
-        
+
         if(s1.charAt(n-1)==s2.charAt(m-1)){
             return dp[n][m]=a;
         }else{
