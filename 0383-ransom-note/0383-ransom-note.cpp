8b99f9d029bfcdef0d410ff9419c0a5f4bf7a68a
Harshit <harshitsaini3582@gmail.com>
2025-04-08 11:19:52 +0100

    Time: 0 ms (100%), Space: 11.8 MB (76.74%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        int cons[]={256};
+        int cons[26]={0};
 
         for(auto c:magazine){
             cons[c-'a']++;
         }
 
         for(auto c:ransomNote){
-            if(cons[c-'a']-->0){
+            if(cons[c-'a']--<=0){
                 return false;
             }
         }
