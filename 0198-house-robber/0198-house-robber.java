b0df28054795f9e986e2bb72e1fa755ac792b252
Harshit <harshitsaini3582@gmail.com>
2025-04-09 18:26:59 +0100

    Time: 0 ms (100%), Space: 41.5 MB (15.25%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,21 +1,20 @@
 class Solution {
-    public int rob(int[] arr) {
-        int inc=arr[0];
-       int exc=0;
-       
-       for(int i=1;i<arr.length;i++){
-           
-           int newnc = arr[i] + exc;
-           int newexc = Math.max(inc,exc);
-           
-           inc=newnc;
-           exc=newexc;
-       }
-       
-       int ans=Math.max(inc,exc);
-       // System.out.println(ans);
-        return ans;
-    }
-    
-}
+    public int rob(int[] nums) {
+        int n=nums.length;
+        int[]dp=new int[n+1];
+        
+        dp[0]=nums[0];
+        
+        for(int i=1;i<n;i++){
+                int take=nums[i];
+                if(i>1){
+                    take+=dp[i-2];
+                }
+            int notTake=0+dp[i-1];
 
+            dp[i]=Math.max(take,notTake);
+        }
+
+        return dp[n-1];
+    }
+}
