304b0fdbfc9fc002c92e1d4d140b9df4e403fe28
Harshit <harshitsaini3582@gmail.com>
2025-04-10 13:46:06 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,55 +1,57 @@
 class Solution {
-    static class Pair implements Comparable<Pair>{
+
+    public class Pair implements Comparable<Pair>{
         int start;
         int end;
-        
+
         Pair(int start,int end){
             this.start=start;
             this.end=end;
         }
-        
+
         public int compareTo(Pair o){
             if(this.start!=o.start){
-                return this.start-o.start;
+               return  this.start-o.start;
             }else{
-                return this.end-o.end;
+                return o.end-this.end;
             }
         }
     }
+
     public int[][] merge(int[][] intervals) {
-        Pair[]pairs=new Pair[intervals.length];
-        
+        Pair []pair=new Pair[intervals.length];
+
         for(int i=0;i<intervals.length;i++){
-            pairs[i]=new Pair(intervals[i][0],intervals[i][1]);
+            pair[i]=new Pair(intervals[i][0],intervals[i][1]);
         }
-        
-        Arrays.sort(pairs);
-        
+
+        Arrays.sort(pair);
+
         Stack<Pair>st=new Stack<>();
-        
-        for(int i=0;i<intervals.length;i++){
+
+       for(int i=0;i<intervals.length;i++){
             if(i==0){
-                st.push(pairs[i]);
+                st.push(pair[0]);
             }else{
                 Pair top=st.peek();
-                
-                if(top.end<pairs[i].start){
-                    st.push(pairs[i]);
+
+                if(top.end<pair[i].start){
+                    st.push(pair[i]);
                 }else{
-                    top.end=Math.max(top.end,pairs[i].end);
+                    top.end=Math.max(top.end,pair[i].start);
                 }
             }
-        }
-        
-        int[][]ans=new int[st.size()][2];
-        
-        for(int i=st.size()-1;i>=0;i--){
-            Pair p=st.pop();
+       }
+
+       int[][]ans=new int[st.size()][2];
+
+       for(int i=st.size()-1;i>=0;i--){
+           Pair p=st.pop();
             
             ans[i][0]=p.start;
             ans[i][1]=p.end;
-        }
-        return ans;
-        
+       }
+
+       return ans;
     }
 }
