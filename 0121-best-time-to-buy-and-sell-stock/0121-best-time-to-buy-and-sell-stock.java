58c4895eeed290adf1f22818527a906252c06276
Harshit <harshitsaini3582@gmail.com>
2025-04-10 22:19:34 +0100

    Time: 620 ms (2.41%), Space: 152.5 MB (7.17%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,15 +1,31 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int maxprofit=0;
-        int mini=prices[0];
+        return helper(0,1,1,prices,new HashMap<String,Integer>());
+    }
 
-        for(int i=1;i<prices.length;i++){
-            int profit=prices[i]-mini;
+    public int helper(int si,int canBuy,int tran,int[]prices,HashMap<String,Integer>hm){
 
-            maxprofit=Math.max(profit,maxprofit);
-            mini=Math.min(mini,prices[i]);
+        if(tran<=0 || si>=prices.length)return 0;
+
+        String key=si+"#"+canBuy+"#"+tran;
+        
+        if(hm.containsKey(key)){
+            return hm.get(key);
         }
 
-        return maxprofit;
+        if(canBuy==1){
+            int idle=helper(si+1,canBuy,tran,prices,hm);
+            int buy=-prices[si] + helper(si+1,0,tran,prices,hm);
+
+            hm.put(key,Math.max(idle,buy));
+            return Math.max(idle,buy);
+        }else{
+            int idle=helper(si+1,canBuy,tran,prices,hm);
+            int sell=prices[si] + helper(si+1,1,-1,prices,hm);
+
+            hm.put(key,Math.max(idle,sell));
+            return Math.max(idle,sell);
+        }
+
     }
 }
