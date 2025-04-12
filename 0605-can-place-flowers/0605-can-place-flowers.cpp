f4d2358de1a58f15a608b6f85a18497b211f3d6d
Harshit <harshitsaini3582@gmail.com>
2025-04-12 22:49:15 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& fb, int n) {
-        int total=0;
+        int total=n;
 
-        for(int i=0;i<fb.size() && total<n;i++){
+        for(int i=0;i<fb.size();i++){
             if(fb[i]==0){
+
                 int next=(i==fb.size()-1)?0:fb[i+1];
-                int prev= (i==0)?0:fb[i-1];
+                int prev=(i==0)?0:fb[i-1];
 
                 if(next==0 && prev==0){
                     fb[i]=1;
                     total++;
                 }
+                
             }
         }
+
         return total==n;
     }
 };
